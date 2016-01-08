a9c0357907af225ef797c4a88e532435ef47c4e1
Bill McDowell <forkunited@gmail.com>
2016-01-08 15:50:17 -0500

    Added document set construction class

---
+++ src/main/java/edu/psu/ist/acs/micro/event/scratch/ConstructMID5NewsDocumentSet.java
@@ -0,0 +1,10 @@
+package edu.psu.ist.acs.micro.event.scratch;
+
+import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLP;
+
+public class ConstructMID5NewsDocumentSet {
+	public static void main(String[] args) {
+		DocumentNLP d = null;
+		System.out.println(d);
+	}
+}
