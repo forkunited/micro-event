02946e127d6e05ad4ad39ebf1da0dae87d585d55
Bill McDowell <forkunited@gmail.com>
2016-03-11 00:09:30 -0500

    ...

--- src/main/java/edu/psu/ist/acs/micro/event/util/EventProperties.java
+++ src/main/java/edu/psu/ist/acs/micro/event/util/EventProperties.java
@@ -1,18 +1,6 @@
 package edu.psu.ist.acs.micro.event.util;
 
-import java.util.Collection;
-import java.util.Map;
-
-import org.bson.Document;
-
-import edu.cmu.ml.rtw.generic.data.Serializer;
-import edu.cmu.ml.rtw.generic.data.annotation.AnnotationType;
-import edu.cmu.ml.rtw.generic.data.annotation.nlp.DocumentNLPInMemory;
-import edu.cmu.ml.rtw.generic.data.store.Storage;
-import edu.cmu.ml.rtw.generic.data.store.StorageFileSystem;
-import edu.cmu.ml.rtw.generic.data.store.StorageMongo;
 import edu.cmu.ml.rtw.generic.util.Properties;
-import edu.psu.ist.acs.micro.event.data.EventDataTools;
 
 /**
  * EventProperties loads and represents a properties
@@ -23,12 +11,6 @@ import edu.psu.ist.acs.micro.event.data.EventDataTools;
  *
  */
 public class EventProperties extends Properties {
-	private String contextInputDirPath;
-	private String experimentOutputDirPath;
-	
-	private String storageMongoMicroEventDatabaseName;
-	private String storageFileSystemMicroEventDirPath;
-	private boolean useMongoStorage;
 	
 	public EventProperties() {
 		this(null);
@@ -37,28 +19,5 @@ public class EventProperties extends Properties {
 	public EventProperties(String path) {
 		super( new String[] { (path == null) ? "event.properties" : path } );
 		
-		this.contextInputDirPath = loadProperty("contextInputDirPath");
-		this.experimentOutputDirPath = loadProperty("experimentOutputDirPath");
-		this.storageFileSystemMicroEventDirPath = loadProperty("storageFileSystemMicroEventDirPath");
-		this.storageMongoMicroEventDatabaseName = loadProperty("storageMongoMicroEventDatabaseName");
-		this.useMongoStorage = Boolean.valueOf(loadProperty("useMongoStorage"));
-	}
-	
-	public String getContextInputDirPath() {
-		return this.contextInputDirPath;
-	}
-	
-	public String getExperimentOutputDirPath() {
-		return this.experimentOutputDirPath;
-	}
-	
-	public Storage<?,Document> getStorage(EventDataTools dataTools, Collection<AnnotationType<?>> annotationTypes) {
-		Map<String, Serializer<?, ?>> serializers = dataTools.getDocumentSerializers(new DocumentNLPInMemory(dataTools), annotationTypes);
-		
-		if (this.useMongoStorage) {
-			return new StorageMongo("mongo", "localhost", this.storageMongoMicroEventDatabaseName, serializers);
-		} else {
-			return new StorageFileSystem<Document>("fs", this.storageFileSystemMicroEventDirPath, serializers);
-		}
 	}
 }
