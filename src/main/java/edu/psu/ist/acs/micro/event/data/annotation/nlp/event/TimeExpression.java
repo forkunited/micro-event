9e1eff973f8b093dd828e4dfde829ea35a3ad5f3
Bill McDowell <forkunited@gmail.com>
2016-03-16 16:40:57 -0400

    ...

--- src/main/java/edu/psu/ist/acs/micro/event/data/annotation/nlp/event/TimeExpression.java
+++ src/main/java/edu/psu/ist/acs/micro/event/data/annotation/nlp/event/TimeExpression.java
@@ -267,7 +267,7 @@ public class TimeExpression implements TLinkable {
 					thisCt = -1;
 				else if (thisCtRelation == TLink.TimeMLRelType.AFTER)
 					thisCt = 1;
-				else if (thisCtRelation == TLink.TimeMLRelType.SIMULTANEOUS || thisCtRelation == TLink.TimeMLRelType.IS_INCLUDED)
+				else if (thisCtRelation == TLink.TimeMLRelType.SIMULTANEOUS)// || thisCtRelation == TLink.TimeMLRelType.IS_INCLUDED)
 					thisCt = 0;
 				else
 					return (unknownVague) ? TLink.TimeMLRelType.VAGUE : null;
@@ -288,7 +288,7 @@ public class TimeExpression implements TLinkable {
 					timeCt = -1;
 				else if (timeCtRelation == TLink.TimeMLRelType.AFTER)
 					timeCt = 1;
-				else if (timeCtRelation == TLink.TimeMLRelType.SIMULTANEOUS || timeCtRelation == TLink.TimeMLRelType.IS_INCLUDED)
+				else if (timeCtRelation == TLink.TimeMLRelType.SIMULTANEOUS)// || timeCtRelation == TLink.TimeMLRelType.IS_INCLUDED)
 					timeCt = 0;
 				else
 					return (unknownVague) ? TLink.TimeMLRelType.VAGUE : null;
