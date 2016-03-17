15c8e6c129cd22085e00cc8ad3e35cc3c70c01bf
Bill McDowell <forkunited@gmail.com>
2016-03-17 00:48:35 -0400

    ...

--- src/main/java/edu/psu/ist/acs/micro/event/task/classify/MethodClassificationTLinkTypeGeneralGovernor.java
+++ src/main/java/edu/psu/ist/acs/micro/event/task/classify/MethodClassificationTLinkTypeGeneralGovernor.java
@@ -131,8 +131,8 @@ public class MethodClassificationTLinkTypeGeneralGovernor extends MethodClassifi
 			if (govDepRel != null) {
 				System.out.println(data.getReferenceName() + " OUTPUT FROM GENGOV : " + type + " " + eGov.getSourceInstanceId() + "->" + eDep.getSourceInstanceId() + "=" + govDepRel.toString());
 				TimeMLRelType rel = govDepRel;
-				if (!e1.getId().equals(eGov.getId()))
-					rel = TLink.getConverseTimeMLRelType(govDepRel);
+				//if (!e1.getId().equals(eGov.getId()))
+				//	rel = TLink.getConverseTimeMLRelType(govDepRel);
 				
 				map.put(datum, rel);
 			}
