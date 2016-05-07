9c1c4209c7df10763a1ac107d64119f9cb3df53a
Bill McDowell <forkunited@gmail.com>
2016-05-07 15:10:11 -0400

    ...

--- src/main/java/edu/psu/ist/acs/micro/event/scratch/ConstructTempEval3.java
+++ src/main/java/edu/psu/ist/acs/micro/event/scratch/ConstructTempEval3.java
@@ -788,7 +788,7 @@ public class ConstructTempEval3 {
 			
 			Event event = new Event(dataTools,
 					eventRef, 
-					eiid,
+					eventRef.getIndexValue(0).toString(),
 					null,
 					null,
 					null,
